ab76f59040dc4d89ee14f3f36f9834ff94dc2270
AururaGrafLeaf <166198555+AururaGrafLeaf@users.noreply.github.com>
2024-07-24 16:32:04 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
 addLayer("A1", {
     name: "POINTS", 
-    symbol: "Ω1", 
+    symbol: "Ω", 
     position: 0, 
     startData() { return {
         unlocked: true,
@@ -27,40 +27,15 @@ addLayer("A1", {
     layerShown(){return true}
 
      
+        
+            
+    }
+
+     
 
     
 })
 
-addLayer("A2", {
-    name: "POINTS", 
-    symbol: "Ω2", 
-    position: 0, 
-    startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
-    }},
-    color: "AF7D12",
-    requires: new Decimal(1e134,237), 
-    resource: "clicky ;]", 
-    baseResource: "points", 
-    baseAmount() {return player.points}, 
-    type: "normal", 
-    exponent: 0.5, 
-    gainMult() { 
-        mult = new Decimal(1)
-        return mult
-    },
-    gainExp() { 
-        return new Decimal(1)
-    },
-    row: 2, 
-    hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
-    layerShown(){return true}
-
-    
-})
 
 addLayer("prestige", {
     name: "prestige POINTS", 
@@ -85,37 +60,6 @@ addLayer("prestige", {
         return new Decimal(1)
     },
     row: 16, 
-    hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
-    layerShown(){return true}
-
-    
-})
-
-addLayer("THIS WILL WAIT", {
-    name: "WAIT FOR THE UPDATE PLSSSS", 
-    symbol: ":D", 
-    position: 0, 
-    startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
-    }},
-    color: "#A53087",
-    requires: new Decimal(10), 
-    resource: "WAITING POINTS", 
-    baseResource: "points", 
-    baseAmount() {return player.points}, 
-    type: "normal", 
-    exponent: 0.5, 
-    gainMult() { 
-        mult = new Decimal(2)
-        return mult 
-    },
-    gainExp() { 
-        return new Decimal(1)
-    },
-    row: 0, 
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
